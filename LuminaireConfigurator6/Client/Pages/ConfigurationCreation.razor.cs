425c7d87e3af28dfa7a0501499e4ae60a8a9fe1f
Emmanuel <emmanueldurin@gmail.com>
2022-02-08 06:57:07 +0100

    First form with validation

---
+++ LuminaireConfigurator6/Client/Pages/ConfigurationCreation.razor.cs
@@ -0,0 +1,14 @@
+﻿using LuminaireConfigurator6.Client.ViewModel;
+
+namespace LuminaireConfigurator6.Client.Pages
+{
+  public partial class ConfigurationCreation
+  {
+    public LuminaireConfiguration Configuration { get; set; } = new LuminaireConfiguration();
+    public void Create()
+    {
+      Console.WriteLine("configuration created");
+    }
+
+  }
+}
---
+++ LuminaireConfigurator6/Client/ViewModel/LuminaireConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuminaireConfigurator6.Client.ViewModel
+{
+  public class LuminaireConfiguration
+  {
+    [Required]
+    public string Name { get; set; } = "";
+    [Range(1,1E10)]
+    public double LampFlux { get; set; }
+    public decimal Price { get; set; }
+    [Required]
+    public string Optic { get; set; } = "";
+    [Range(1,1E6)]
+    public int LampColor { get; set; }
+  }
+}
