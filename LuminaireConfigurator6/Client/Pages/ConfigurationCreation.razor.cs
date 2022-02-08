71d7636942439cdc0cbf1b8ec6a2623a87a2a32e
Emmanuel <emmanueldurin@gmail.com>
2022-02-08 07:00:21 +0100

    Add selects

--- LuminaireConfigurator6/Client/Pages/ConfigurationCreation.razor.cs
+++ LuminaireConfigurator6/Client/Pages/ConfigurationCreation.razor.cs
@@ -5,10 +5,11 @@ namespace LuminaireConfigurator6.Client.Pages
   public partial class ConfigurationCreation
   {
     public LuminaireConfiguration Configuration { get; set; } = new LuminaireConfiguration();
+    public int[] LampColors { get; set; } = new int[] { 2200, 2700, 3000, 4000, 5700 };
+    public string[] Optics { get; set; } = new string[] { "ON10", "ON11", "OL10", "OL11", "OM10", "OM11" };
     public void Create()
     {
       Console.WriteLine("configuration created");
     }
-
   }
 }
