f6d333d27e3248f35c0e903dfebfdf120ca137a7
Emmanuel <emmanueldurin@gmail.com>
2022-02-09 10:18:46 +0100

    Fix bug and separate CustomSelect.razor.cs code

---
+++ LuminaireConfigurator6/Client/Shared/CustomSelect.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LuminaireConfigurator6.Client.Shared
+{
+  public partial class CustomSelect<TItem,TValue,TDisplay> : ComponentBase
+  {
+    [Parameter]
+    public int Size { get; set; } = 1;
+    [Parameter]
+    public TItem? Selected { get; set; }
+    [Parameter]
+    public IEnumerable<TItem> Items { get; set; } = Enumerable.Empty<TItem>();
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+    [Parameter]
+    public Func<TItem, TValue>? ValueSelector { get; set; }
+    [Parameter]
+    public Func<TItem, TDisplay>? DisplaySelector { get; set; }
+    [Parameter]
+    public EventCallback<TItem> SelectedChanged { get; set; }
+
+    private TValue? selectedValue { get; set; }
+    public TValue? SelectedValue
+    {
+      get => selectedValue;
+      set
+      {
+        selectedValue = value;
+
+        Selected = Items==null
+          ?default(TItem?)
+          :Items.FirstOrDefault(i => ValueSelector != null && ValueSelector(i)?.ToString()?.Equals(value?.ToString()) == true);
+        SelectedChanged.InvokeAsync(Selected);
+      }
+    }
+  }
+}
