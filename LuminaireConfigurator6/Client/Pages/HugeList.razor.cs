370c87d398dd3b841276a3db5e10f533fcfaf921
Emmanuel <emmanueldurin@gmail.com>
2022-02-10 18:42:58 +0100

    HugeList that takes much memory

---
+++ LuminaireConfigurator6/Client/Pages/HugeList.razor.cs
@@ -0,0 +1,32 @@
+﻿using LuminaireConfigurator6.Shared.Model;
+
+namespace LuminaireConfigurator6.Client.Pages
+{
+  public partial class HugeList
+  {
+    private LuminaireConfiguration[]? luminaireConfigurations;
+    public LuminaireConfiguration[]? LuminaireConfigurations
+    {
+      get => luminaireConfigurations;
+      set => luminaireConfigurations = value;
+    }
+    protected override Task OnInitializedAsync()
+    {
+      Console.WriteLine("Before Enumerable.Range");
+      LuminaireConfigurations = Enumerable.Range(1, 500_000)
+                                          .Select(
+                                            i => new LuminaireConfiguration
+                                            {
+                                              Id = i,
+                                              Name = "Luminaire" + i,
+                                              CreationTime = DateTime.Now,
+                                              LampColor = 3000 + (i % 2000),
+                                              Optic = "OM" + (i % 10),
+                                              LampFlux = 1000,
+                                            })
+                                          .ToArray();
+      Console.WriteLine("After Enumerable.Range");
+      return Task.FromResult(0);
+    }
+  }
+}
