6d9391cf9600892b986d9b8aeff639a591f09215
Emmanuel Durin <emmanueldurin@gmail.com>
2024-01-09 09:42:43 +0100

    Add comments

--- LuminaireConfigurator6/Client/Shared/CustomSelect.razor.cs
+++ LuminaireConfigurator6/Client/Shared/CustomSelect.razor.cs
@@ -7,24 +7,30 @@ namespace LuminaireConfigurator6.Client.Shared
     public partial class CustomSelect<TItem, TValue, TDisplay>
                                       : ComponentBase
     {
+        // EditContext is inherited from the EditForm
         [CascadingParameter]
         private EditContext? CascadedEditContext { get; set; }
         protected internal FieldIdentifier FieldIdentifier { get; set; }
         private TItem? selected;
-        [Parameter, EditorRequired]
-        public TItem? Selected
-        {
-            get => selected;
-            set => selected = value;
-        }
         [Parameter] public EventCallback<TItem> SelectedChanged { get; set; }
 
         private Expression<Func<TItem>>? selectedExpression { get; set; }
+        // {PropertyName}Expression if the convention for adequate databinding of property
+        // selectedExpression is useful later for getting the name of the property  in 
+        // order to get the name of FieldIdentifier in SetParametersAsync
         [Parameter]
         public Expression<Func<TItem>>? SelectedExpression
         {
             get => selectedExpression;
             set => selectedExpression = value;
+        }
+        // A standardly named property is also in the convention
+        // for easy to use databinding
+        [Parameter, EditorRequired]
+        public TItem? Selected
+        {
+            get => selected;
+            set => selected = value;
         }
         [Parameter, EditorRequired]
         public Func<TItem, TDisplay> DisplaySelector { get; set; } = null!;
